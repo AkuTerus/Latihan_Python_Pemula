8c0ac5d04ca340c67722c1b944ea2056c0302358
AkuTerus <66345580+AkuTerus@users.noreply.github.com>
2022-12-08 23:23:04 +0700

    Delete hitungangka.py

--- hitungangka.py
+++
@@ -1,9 +0,0 @@
-angka = input("Masukkan angka: ")
-angka_hitung = input("Masukkan angka yang dihitung: ")
-
-muncul = 0
-for i in range(len(angka)):
-    if angka[i] == angka_hitung:
-        muncul += 1
-        
-print(f"angka {angka_hitung} muncul sebanyak {muncul} kali")
