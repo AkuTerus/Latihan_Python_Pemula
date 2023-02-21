b71cd1107a6cd2db336eddecd21615b75d7230f7
AkuTerus <66345580+AkuTerus@users.noreply.github.com>
2023-02-21 11:58:45 +0700

    Add files via upload

---
+++ 21-02-23.py
@@ -0,0 +1,29 @@
+# masukanAngka = int(input('Masukan Angka 0 - 9999 :  '))
+# total = 0 
+
+# while(masukanAngka > 0):
+#     digit = masukanAngka % 10
+#     total += digit
+#     masukanAngka //= 10
+# print(f'Total Semua digit adalah {total}')
+
+# uang = int(input("Masukan Jumlah Uang ")) 
+# rupiahkeUsd = uang / 14900
+# rupiahkeYen = uang / 130
+# rupiahkeEur = uang / 16700
+# rupiahkeAud = uang / 11500
+# rupiahkeUsd=float(rupiahkeUsd)
+# print('Jumlah ',uang,'adalah'(rupiahkeUsd),'USD')
+# print(rupiahkeUsd)
+# print(f'Jumlah {rupiah} rupiah adalah {yen} YEN')
+# print(f'Jumlah {rupiah} rupiah adalah {eur} EUR')
+# print(f'Jumlah {rupiah} rupiah adalah {aud} AUD')
+
+Kode='AB123'
+NamaBarang='Kaca Mata'
+HargaSatuan=125000
+Stok=10
+print('Kode barang %s \n Nama Barang= %s \n Harga Satuan=Rp %d \n Stok Barang=%d' %
+(Kode,NamaBarang,HargaSatuan,Stok))
+HargaBarang=float(HargaSatuan)
+print('Harga Satuan Barang= Rp. %.3f' %(HargaBarang))
